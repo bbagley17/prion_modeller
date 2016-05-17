ca207acdc0a96168cd18de426e9b25f99dbbc50e
bbagley17 <bbagley17@my.whitworth.edu>
2016-05-17 16:34:25 -0700

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -5,45 +5,32 @@
 
 using namespace std;
 
-void Cell::checkCollision(vector<Vesicle>& vesicleVector)
+void Cell::checkCollision(vector<Vesicle>& vesicleVector, int& vesicleCount)
 {
 	//Scans through entire vesicle vector to see if any vesicles have collided with the cell
 	for (int n = 0; n < vesicleVector.size(); n++)
 	{
+	//
+		//
+		//
+		//Change this to check if the vesicle is within a range of the cell's coordinates. As is, of course it doesn't work.
 		if ((vesicleVector[n].xCrd == xCoord) && (vesicleVector[n].yCrd == yCoord) && (vesicleVector[n].zCrd == zCoord))
 		{
-			//Something that deletes the vector and adds its prions to the cell.
+			prionCount += vesicleVector[n].prionCnt;
+			vesicleVector.erase(vesicleVector.begin() + n);
+			vesicleCount--;
 		}
 	}
 }
 
 void Cell::createVesicle(vector<Vesicle>& vesicleVector, int& vesicleCount)
 {
-	if ((prionCount / (3.14159 * radius * radius) < maxPrionDensity)) //parameter for the max number of prions/volume a cell can hold)
-	{
+	
 		int content = 20000; //Exosomes contain an average of roughly 20,000 proteins.
-		double xStart;
-		double yStart;
-		double zStart;
-		do
-		{
-
-			xStart = rand()%(2*radius);
-			yStart = rand()%(2*radius);
-			zStart = rand()%(2*radius);
-		} while ((sqrt((xStart * xStart)+(yStart * yStart) + (zStart * zStart)) > radius) && 
-			(sqrt((xStart * xStart) + (yStart * yStart) + (zStart * zStart)) <= (radius + 0.1 *radius))); 
-		/* This section above is a bit complicated. It's a rough approximation, but works. 
-			Basically, the initial position of the vesicle is set randomly within two radii of the cell.
-			If that position is outside the cells outer surface (more than 1 radius), but within 110% of the radius,
-			the vesicle will be created at that site. If not, the coordinates will be reiterated. 
-
-			It is important to note that the above is just a measure of magnitude. that is, it solves for a set of xyz magnitudes
-			beyond the cell membrane. Below, these magnitudes will be given direction.
-		*/
+		double xStart = 600;
+		double yStart = 600;
+		double zStart = 600;
 		
-		// dir is randomly selected between 1 and six. This determines which of the six sites with the above xyz magnitudes at which
-			//the vesicle will actually be created
 		int dir = rand()%8;
 
 		if (dir == 0)
@@ -100,7 +87,7 @@ void Cell::createVesicle(vector<Vesicle>& vesicleVector, int& vesicleCount)
 		vesicleCount++;
 		//Removing the excreted prions from the cell's tally
 		prionCount = prionCount - content;
-	}
+	
 }
 
 //Call ODE solver to model prion spread within cell
