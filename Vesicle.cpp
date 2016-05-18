34e9927e58c8ef19cab4af8254feeaad48ea890a
bbagley17 <bbagley17@my.whitworth.edu>
2016-05-17 19:52:46 -0700

    Update Vesicle.cpp

--- Vesicle.cpp
+++ Vesicle.cpp
@@ -8,29 +8,31 @@ using namespace std;
 void Vesicle::randomWalk()
 {
 	//Values for whether the vesicle will move in the positive or negative x,y,z directions, respetively
-	int xPlusMinus;
-	int yPlusMinus;
-	int zPlusMinus;
+	int xPlusMinus = 0;
+	int yPlusMinus = 0;
+	int zPlusMinus = 0;
 	//Magnitudes of the movement the vesicle will perform in each direction
 	double xMagnitude;
 	double yMagnitude;
 	double zMagnitude;
 
-	//For each "plusminus" value, if the value is 0, the vesicle moves in "-" direction. If 1, vesicle moves in "+" direction 
-	xPlusMinus = rand() % 2;
-	yPlusMinus = rand() % 2;
-	zPlusMinus = rand() % 2;
+	//For each "plusminus" value, if the value is 0, the vesicle moves in "-" direction. If 1, vesicle moves in "+" direction
+	// if the value is 2, the vesicle is stationary in that axis
+	xPlusMinus = rand() % 3;
+	yPlusMinus = rand() % 3;
+	zPlusMinus = rand() % 3;
 
 	//Set the magnitude of the random step in each direction as some decimal between 0 and 1
-	xMagnitude = 100;//(rand() % 100) / 100;
-	yMagnitude = 100;//(rand() % 100) / 100;
-	zMagnitude = 100;//(rand() % 100) / 100;
+	xMagnitude = 10;//(rand() % 100) / 100;
+	yMagnitude = 10;//(rand() % 100) / 100;
+	zMagnitude = 10;//(rand() % 100) / 100;
 
 	//Updating each coordinate as a random walk
 	if (xPlusMinus = 0)
 		xCrd = xCrd - xMagnitude;
 	if (xPlusMinus = 1)
 		xCrd = xCrd + xMagnitude;
+
 
 	if (yPlusMinus = 0)
 		yCrd = yCrd - yMagnitude;
